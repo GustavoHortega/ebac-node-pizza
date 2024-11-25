71f5a338fadb11b4c42f7cd18649662348639c1e
Gustavo Hortega <gustavohortega80@gmail.com>
2024-11-25 12:45:21 -0400

    importe o eschema de pedidos.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,12 @@
 const mongoose =  require('mongoose');
+const Pedido = require('./pedido');
 
 const connect = () =>{
     mongoose.connect('mongodb://localhost:27017/ebacpizza')
 }
 
 module.exports = {
-    connect
+    connect,
+    
+    Pedido
 }
