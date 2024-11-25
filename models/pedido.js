20368a53f8f87faebdd585c81945eceef5727b33
Gustavo Hortega <gustavohortega80@gmail.com>
2024-11-25 12:49:07 -0400

    construa o schema para um pedido.

--- models/pedido.js
+++ models/pedido.js
@@ -1,1 +1,48 @@
 const { Schema } = require('mongoose');
+
+const Pedido = new Schema({
+    cliente:{
+        nome:{
+            type: String,
+            required: true
+        },
+        sobrenome:{
+            type: String,
+            required: true
+
+        }
+    },
+    pizzas:[
+        {
+            sabor:{
+                type: String,
+                required: true
+            },
+            quantidade:{
+                type: Number,
+                required: true
+            },
+            tamanho:{
+                type: String,
+                required: true,
+                enum: ['Pequeno','Médio','Grande',]
+            }
+        }
+    ],
+    metodoDePagamento:{
+        type: String,
+        required: true,
+        enum: ['PIX','Cartão de Crédito']
+    },
+    total:{
+        type: Number,
+        required: true
+    },
+    pago:{
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+module.exports = Pedido;
